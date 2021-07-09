51b1e48ee47cc2e7b760f29d6a6c857e0662487d
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-07-08 18:32:20 -0700

    week1_update

--- week1/index.js
+++ week1/index.js
@@ -1,10 +1,13 @@
-fetch('menu.json')
-    .then((Response) =>{
-        return Response.json()
-    })
-    .then((menu)=>{
-        console.log(menu)
-    })
-    .catch((err)=>{
-        return "menu unavailable"
-    })
+//fetch('menu.json')
+//    .then((Response) =>{
+//        return Response.json()
+//    })
+//    .then((menu)=>{
+//        console.log(menu)
+//    })
+//    .catch((err)=>{
+//        return "menu unavailable"
+//    })
+var data = require("./menu.json")
+console.log(data.menu)
+//console.log(data.menu.appetizers)
