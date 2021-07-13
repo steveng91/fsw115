d0614783e7c71ad17aee9af5eeae62b35b13c1f6
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-07-13 13:52:38 -0700

    week2_commit

---
+++ week2/week2_assignment/index.js
@@ -0,0 +1,63 @@
+var statSheet = [
+    fname = "name: Luke Skywalker",
+    height ="height: 172",
+    mass ="mass: 77",
+    hair_color="hair_color: blond",
+    skin_color="skin_color: fair",
+    eye_color="eye_color: blue",
+    birth_year="birth_year: 19BBY",
+    gender="gender: male",
+    homeworld="homeworld: https://swapi.dev/api/planets/1/"]
+    document.getElementById("listItem1").innerHTML =fname
+    document.getElementById("listItem2").innerHTML =height
+    document.getElementById("listItem3").innerHTML =mass
+    document.getElementById("listItem4").innerHTML =hair_color
+    document.getElementById("listItem5").innerHTML =skin_color
+    document.getElementById("listItem6").innerHTML =eye_color
+    document.getElementById("listItem7").innerHTML =birth_year
+    document.getElementById("listItem8").innerHTML =gender
+    document.getElementById("listItem9").innerHTML =homeworld
+ 
+   
+var films = [
+      film1="film 1:https://swapi.dev/api/films/1/",
+      film2="film 2:https://swapi.dev/api/films/2/",
+      film3="film 3:https://swapi.dev/api/films/3/",
+      film4="film 6:https://swapi.dev/api/films/6/"
+]
+document.getElementById('film1').innerHTML=film1
+document.getElementById('film2').innerHTML=film2
+document.getElementById('film3').innerHTML=film3
+document.getElementById('film4').innerHTML=film4
+
+
+species = [species = "species:"]
+ document.getElementById('species1').innerHTML =species
+
+ vehicles = [
+      vehicle1="https://swapi.dev/api/vehicles/14/",
+      vehicle2="https://swapi.dev/api/vehicles/30/"
+]
+document.getElementById('vehicle1').innerHTML=vehicle1
+document.getElementById('vehicle2').innerHTML=vehicle2
+
+starships = [
+     starship1= "https://swapi.dev/api/starships/12/",
+     starship2="https://swapi.dev/api/starships/22/"
+]
+document.getElementById('starship1').innerHTML=starship1
+document.getElementById('starship2').innerHTML=starship2
+
+var source = [
+      created="created : 2014-12-09T13:50:51.644000Z",
+      edited="edited: 2014-12-20T21:17:56.891000Z",
+      url="url: https://swapi.dev/api/people/1/"]
+document.getElementById('created').innerHTML=created
+document.getElementById('edited').innerHTML=edited
+document.getElementById('url').innerHTML=url
+//let stats = ""
+//for (let x in statSheet){
+  //  stats += statSheet[x] + ""
+
+//document.getElementById('charStats').innerHTML = stats
+//}
