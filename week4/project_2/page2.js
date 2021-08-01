33e00a6c2c15ddebfe8d2f74f5c7cb6cef711b57
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-08-01 12:42:31 -0700

    week4_update2

--- week4/project_2/page2.js
+++ week4/project_2/page2.js
@@ -1,7 +1,6 @@
-
-var btn = document.getElementById('list')
-btn.addEventListener('click', function(){
-    const swapi = axios.get('https://swapi.dev/api/people')
+var btn2 = document.getElementById('btn3')
+btn2.addEventListener('click', function(){
+    const pokemon = axios.get('https://pokeapi.co/api/v2/pokemon')
     .then(response=>{
         console.log(response.data)
        list(response.data)
@@ -9,7 +8,6 @@ btn.addEventListener('click', function(){
     .catch(error=>{console.log(error)})
    function list(data){
        console.log(data)
-      // console.log(response.data,response.data.length)
        for (let i = 0; i < data.results.length; i++) {
            const li = document.createElement('li')
            li.textContent = data.results[i].name
